95db3a47e054633cbcfa55e248eb936a36949643
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-11-24 13:07:20 -0500

    added working form that saves to db

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,2 +1,15 @@
-
+// call firebase rt db
+var db = firebase.database();
+var contactRef = db.ref();
+// add onsubmit handler 
+var contactForm = document.getElementById('formContactMe');
+contactForm.onsubmit = function (event) {
+    var formData = new FormData(contactForm);
+    // call firebase
+    contactRef.push({
+        name: formData.get('inputName'),
+        email: formData.get('inputEmail'),
+        comments: formData.get('inputComments')
+    })
+}
 
