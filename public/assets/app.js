9b4fd6f29bd49b0db18a6e2f82c6936f86c8d6d6
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-11-26 17:15:28 -0500

    added defer on app js script

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,5 +1,25 @@
 // call auth 
-var userAuth = firebase.auth(); 
+var userAuth = firebase.auth();
+// use auth 
+userAuth.signInAnonymously().catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
+
+userAuth.onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        // ...
+    } else {
+        // User is signed out.
+        // ...
+    }
+    // ...
+});
 // call firebase rt db
 var db = firebase.database();
 var contactRef = db.ref();
